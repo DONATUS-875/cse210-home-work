446b7f7bf0168d4175a3653f6d5193f77c5b3437
DONATUS-875 <donatusahule40@gmail.com>
2026-03-28 10:24:09 +0100

    Add week04 assignments: OnlineOrdering and YouTubeVideos

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("456 Broadway Avenue", "Brooklyn", "NY", "USA");
+        Customer customer1 = new Customer("Alice Johnson", address1);
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Laptop", "P001", 1200, 1));
+        order1.AddProduct(new Product("Mouse", "P002", 25, 2));
+
+        Address address2 = new Address("45 King Street", "London", "London", "UK");
+        Customer customer2 = new Customer("James Smith", address2);
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Headphones", "P003", 100, 1));
+        order2.AddProduct(new Product("Keyboard", "P004", 75, 1));
+        order2.AddProduct(new Product("Monitor","P005", 300, 2));
+
+        List<Order> orders = new List<Order> {order1, order2};
+
+        foreach (Order order in orders)
+        {
+            Console.WriteLine(order.GetPackingLabel());
+            Console.WriteLine(order.GetShippingLabel());
+            Console.WriteLine($"Total cost: ${order.GetTotalCost()}\n");
+        }
     }
+
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("Learning C#","Alice", 600 );
+        video1.AddComment(new Comment("John", "Great explaination!"));
+        video1.AddComment(new Comment("Mary", "Very helpful thanks"));
+        video1.AddComment(new Comment("Sam", "Can you give us the link?"));
+        videos.Add(video1);
+
+
+        Video video2 = new Video("Travel Vlog: Paris", "Sophia", 900);
+        video2.AddComment(new Comment("Chris", "Beautiful shots"));
+        video2.AddComment(new Comment("Ella", "I want to visit someday."));
+        video2.AddComment(new Comment("Tom", "What camera are you using?"));
+        videos.Add(video2);
+
+        Video video3 = new Video("Cocking Pasta", "Chef Mike", 300);
+        video3.AddComment(new Comment("Anna", "Looks delicious"));
+        video3.AddComment(new Comment("Leo", "I tried this recipe, amazing."));
+        video3.AddComment(new Comment("Nina", "Can you share a vegeterian version?"));
+        videos.Add(video3);
+
+
+        foreach (Video video in videos)
+        {
+            video.DisplayVideoInfo();
+        }
+
+
+
+
     }
 }
